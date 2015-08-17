af7fb8301b30a02b9c11c1d9a3d18dd3cb6d78d8
System Administrator <root@Leos-iMac.fios-router.home>
2015-08-16 23:33:03 -0400

    updated article stuff

--- analyze_article_text.py
+++ analyze_article_text.py
@@ -10,7 +10,8 @@ cur_dir = path.dirname(__file__)
 article_text = open(path.join(cur_dir, 'article_text.txt')).read()
 ampelman_mask = imread(path.join(cur_dir, 'ampelman_mask.png'))
 
-word_cloud = WordCloud(mask=ampelman_mask, background_color='white', stopwords=STOPWORDS.add("m"))
+word_cloud = WordCloud(mask=ampelman_mask, background_color='white', stopwords=STOPWORDS.add("m"),
+						height=720, width=1080)
 
 word_cloud.generate(article_text)
 
